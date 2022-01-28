1e1e50163d0955481331f7d3fdfcccac5acb493c
Paulo Rodrigues <paulo-roberto-caetano-rodrigues@impulso.network>
2022-01-28 18:46:22 -0300

    add config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+  ],
   controllers: [AppController],
   providers: [],
 })
