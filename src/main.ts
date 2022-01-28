d6e0de47bab0e5bd65327163019617769b798cae
Paulo Rodrigues <paulo-roberto-caetano-rodrigues@impulso.network>
2022-01-28 18:43:28 -0300

    add middleware log

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NextFunction } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const writeLog = (req: Request, res: Response, next: NextFunction) => {
+    console.log(`Rota acessada: ${req.url}`);
+    console.log(`Metodo: ${req.method}`);
+    console.log(`header: ${JSON.stringify(req.headers)}`);
+    next();
+  };
+
+  app.use(writeLog);
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
