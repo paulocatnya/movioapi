fc8fd7338738e6faafaf937b4e0c6a28879c40a5
Paulo Rodrigues <paulo-roberto-caetano-rodrigues@impulso.network>
2022-01-28 18:50:52 -0300

    add database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AuthModule } from './modules/auth/auth.module';
+import { MovimentsModule } from './modules/moviment/moviments.module';
+import { UsersModule } from './modules/user/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
@@ -8,7 +12,23 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       isGlobal: true,
       envFilePath: '.env',
     }),
+    UsersModule,
+    MovimentsModule,
+    AuthModule,
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get('DATABASE_URL'),
+        entities: [__dirname + '/modules/**/*.entity{.ts,.js}'],
+        migrations: [__dirname + '/migrations/*{.ts,.js}'],
+        synchronize: false,
+        migrationsRun: false,
+        logging: true,
+      }),
+      inject: [ConfigService],
+    }),
   ],
+
   controllers: [AppController],
   providers: [],
 })
