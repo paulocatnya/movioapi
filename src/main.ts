e0792bea79b562182ace05ea0c0d63cff07c9c34
Paulo Rodrigues <paulo-roberto-caetano-rodrigues@impulso.network>
2022-01-28 22:39:42 -0300

    add log and cors

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,12 @@ import { NextFunction } from 'express';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+
   const writeLog = (req: Request, res: Response, next: NextFunction) => {
     console.log(`Rota acessada: --> ${req.url}`);
     console.log(`Metodo: --> ${req.method}`);
+    console.log(`body: --> ${JSON.stringify(req.body)}`);
     console.log(`header: --> ${JSON.stringify(req.headers)}`);
     next();
   };
