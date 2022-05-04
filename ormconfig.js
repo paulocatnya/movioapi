27cc5e06833f2e41c026599fff8221584132749b
Paulo Rodrigues <paulo-roberto-caetano-rodrigues@impulso.network>
2022-05-04 18:02:01 -0300

    ssl rule

--- ormconfig.js
+++ ormconfig.js
@@ -1,12 +1,12 @@
 console.log('process.env.DATABASEE_URL:>> ', process.env.DATABASE_URL);
+console.log('process.env.IS_DEVELOPMENT:>> ', process.env.IS_DEVELOPMENT);
+
 module.exports = {
   type: 'postgres',
   url: process.env.DATABASE_URL,
   entities: ['dist/**/*{.entity,.index}{.ts,.js}'],
   migrations: ['dist/migrations/*{.ts,.js}'],
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  ssl: process.env.IS_DEVELOPMENT === "TRUE" ? false : true,  
   cli: {
     migrationsDir: ['src/database/migrations/'],
     entitiesDir: 'src/models',
